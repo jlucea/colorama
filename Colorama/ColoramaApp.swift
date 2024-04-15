195f043ee6742e2908f74c866b546aef5eacd544
Jaime Lucea <jlucea@eservicios.indra.es>
2024-04-15 08:27:20 +0200

    Initial Commit

---
+++ Colorama/ColoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoramaApp.swift
+//  Colorama
+//
+//  Created by Jaime Lucea on 15/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ColoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Colorama
+//
+//  Created by Jaime Lucea on 15/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
